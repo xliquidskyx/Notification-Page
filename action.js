3df37260f27b96f46f0db90051415b0ceac807ed
Aventia <lauralis0405@gmail.com>
2022-09-29 17:17:56 +0200

    css made responsive for bigger devices

---
+++ action.js
@@ -0,0 +1,4 @@
+var activeNotifications = document.querySelectorAll('.active');
+var numberOfNotifications = activeNotifications.length;
+
+document.querySelector(".notification-number").innerHTML = numberOfNotifications;
