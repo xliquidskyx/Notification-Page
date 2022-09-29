8e18abb1963e8b482c5309a7d7bc15d8546e2ddd
Aventia <lauralis0405@gmail.com>
2022-09-29 18:19:27 +0200

    user can now mark all active notifications as read

--- action.js
+++ action.js
@@ -1,4 +1,30 @@
 var activeNotifications = document.querySelectorAll('.active');
 var numberOfNotifications = activeNotifications.length;
+var allActiveNotifications = document.querySelectorAll('.notification.active');
+var redDot = document.querySelectorAll('.notif-dot');
+var allNotifications = document.querySelectorAll('.notifications');
+var notificationNumber = document.querySelector(".notification-number");
+notificationNumber.innerHTML = numberOfNotifications;
 
-document.querySelector(".notification-number").innerHTML = numberOfNotifications;
+$('.mark-all-btn').click(function () {
+
+    if (numberOfNotifications != 0) {
+        $('.mark-all-btn').text("Mark all as unread");  
+    
+        allActiveNotifications.forEach(function (element) {
+            element.classList.remove('active'); //removes all active classes
+        })
+
+        numberOfNotifications = 0;
+        notificationNumber.innerHTML = 0;
+
+    } else {
+        $('.mark-all-btn').text("Mark all as read");
+        allNotifications.forEach(function (element) {
+            $(element).classList.add('active'); //todo make it work
+        })
+    }
+
+    
+
+})
